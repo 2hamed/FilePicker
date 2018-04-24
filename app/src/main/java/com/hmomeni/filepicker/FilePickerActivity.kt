a96791b7d22d70b3c5fd79b6ba47d7a80750b868
hamed <hamed.m7@gmail.com>
2018-04-24 16:03:29 +0430

    added media_type

--- app/src/main/java/com/hmomeni/filepicker/FilePickerActivity.kt
+++ app/src/main/java/com/hmomeni/filepicker/FilePickerActivity.kt
@@ -1,6 +1,8 @@
 package com.hmomeni.filepicker
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Environment
@@ -8,30 +10,68 @@ import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_file_picker.*
 import java.io.File
 import java.io.FileFilter
 import java.util.*
 import java.util.regex.Pattern
 
-class FilePickerActivity : AppCompatActivity(), FPItemClickCallback {
+class FilePickerActivity : AppCompatActivity(), FPItemClickCallback, AdapterView.OnItemSelectedListener {
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        if (spinnerInited) {
+            applyMediaType(position)
+            initPicker()
+        } else {
+            spinnerInited = true
+        }
+    }
 
 
     val MY_WRITE_EXTERNAL_STORAGE_PERMISSION_REQUEST = 6823
     val fpItems = mutableListOf<FPItem>()
     val levels = Stack<String>()
-    val fileRegex = Pattern.compile("^(png|jpe?g|gif)$", Pattern.CASE_INSENSITIVE).toRegex()
+    lateinit var fileRegex: Regex
     var currentPath = ""
+    var spinnerInited = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_file_picker)
+
+        val mediaType = intent.getIntExtra("media_type", 0)
+        applyMediaType(mediaType)
+
         checkPermission()
 
+        typeSpinner.adapter = ArrayAdapter.createFromResource(this, R.array.media_types, android.R.layout.simple_spinner_item).also {
+            it.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        }
+        typeSpinner.onItemSelectedListener = this
+    }
+
+    fun applyMediaType(mediaType: Int) {
+        when (mediaType) {
+        //photos
+            0 -> fileRegex = Pattern.compile("^(png|jpe?g|gif)$", Pattern.CASE_INSENSITIVE).toRegex()
+        //videos
+            1 -> fileRegex = Pattern.compile("^(mp4|mpe?g|3gp)$", Pattern.CASE_INSENSITIVE).toRegex()
+        //audios
+            2 -> fileRegex = Pattern.compile("^(mp3)$", Pattern.CASE_INSENSITIVE).toRegex()
+        //all
+            3 -> fileRegex = Pattern.compile("^(.*)$", Pattern.CASE_INSENSITIVE).toRegex()
+        }
 
     }
 
     private fun initPicker() {
+        levels.empty()
         getDirContent(Environment.getExternalStorageDirectory().absolutePath)
     }
 
@@ -41,12 +81,16 @@ class FilePickerActivity : AppCompatActivity(), FPItemClickCallback {
         fpItems.clear()
         val dir = File(path)
         for (node in dir.listFiles(FileFilter { it.isDirectory or it.extension.matches(fileRegex) })) {
-            fpItems.add(FPItem(
-                    node.absolutePath,
-                    if (node.isDirectory) 1 else 0,
-                    node.name,
-                    if (node.isDirectory) node.listFiles(FileFilter { it.isDirectory or it.extension.matches(fileRegex) }).size else 0)
-            )
+            val childCount = if (node.isDirectory) node.listFiles(FileFilter { it.isDirectory or it.extension.matches(fileRegex) }).size else 0
+            if (!node.isDirectory || childCount != 0) {
+                fpItems.add(FPItem(
+                        node.absolutePath,
+                        if (node.isDirectory) 1 else 0,
+                        node.name,
+                        childCount)
+                )
+            }
+
         }
 
         populateRecyclerView()
@@ -66,6 +110,9 @@ class FilePickerActivity : AppCompatActivity(), FPItemClickCallback {
         if (fpItems[position].type == 1) {
             levels.push(currentPath)
             getDirContent(fpItems[position].path)
+        } else {
+            setResult(Activity.RESULT_OK, Intent().putExtra("file_path", fpItems[position].path))
+            finish()
         }
     }
 
--- app/src/main/java/com/hmomeni/filepicker/MainActivity.kt
+++ app/src/main/java/com/hmomeni/filepicker/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.hmomeni.filepicker
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startActivity(Intent(this, FilePickerActivity::class.java))
+        browseBtn.setOnClickListener {
+            startActivityForResult(Intent(this, FilePickerActivity::class.java).putExtra("media_type", 1), 1234)
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            1234 -> {
+                if (resultCode == Activity.RESULT_OK)
+                    GlideApp.with(this).load(data!!.getStringExtra("file_path")).into(imageView)
+            }
+        }
     }
 }

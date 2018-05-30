9c10f79463298011345de00cec1974b5977b264e
hamed <hamed.ma7@gmail.com>
2018-05-30 11:57:36 +0430

    added switch camera buttons

--- app/src/main/java/com/hmomeni/filepicker/CameraActivity.kt
+++ app/src/main/java/com/hmomeni/filepicker/CameraActivity.kt
@@ -2,17 +2,16 @@ package com.hmomeni.filepicker
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.hmomeni.filepicker.cameraview.cameraview.CameraView
 import kotlinx.android.synthetic.main.activity_camera.*
 
-class CameraActivity : AppCompatActivity() {
+class CameraActivity : AppCompatActivity(), View.OnClickListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
-        captureBtn.setOnClickListener {
-            cameraView.takePicture()
-        }
 
         cameraView.addCallback(object : CameraView.Callback() {
             override fun onCameraOpened(cameraView: CameraView?) {
@@ -27,6 +26,10 @@ class CameraActivity : AppCompatActivity() {
                 super.onPictureTaken(cameraView, data)
             }
         })
+
+
+        switchCameraBtn.setOnClickListener(this)
+        captureBtn.setOnClickListener(this)
     }
 
     override fun onStart() {
@@ -37,5 +40,21 @@ class CameraActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         cameraView.stop()
+    }
+
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.switchCameraBtn -> if (cameraView.facing == CameraView.FACING_BACK) {
+                cameraView.facing = CameraView.FACING_FRONT
+                switchCameraBtn.setImageResource(R.drawable.ic_camera_back)
+            } else {
+                cameraView.facing = CameraView.FACING_BACK
+                switchCameraBtn.setImageResource(R.drawable.ic_camera_back)
+            }
+            R.id.captureBtn -> {
+                cameraView.takePicture()
+            }
+        }
     }
 }
